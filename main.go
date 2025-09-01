9a34f9c551902e713555e32fe048bf7557444f7d
Filip M <filipm7@protonmail.com>
2025-09-01 16:30:47 +0200

    feat: initial workflow, does the entier install

--- main.go
+++ main.go
@@ -4,23 +4,37 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"io/fs"
+	"log"
 	"net/http"
+	"os"
+	"os/exec"
+	"slices"
+	"strings"
 )
 
 type Args struct {
-	s bool
-	q bool
+	S bool
 }
 
 var args Args
 
 func main() {
-	flag.BoolVar(&args.s, "S", false, "Do the sync operation")
+	flag.BoolVar(&args.S, "S", false, "Do the sync operation")
 	flag.Parse()
 	packageNames := flag.Args()
 
+	installLoc := "/tmp/ygh"
+	if _, err := os.Stat(installLoc); os.IsNotExist(err) {
+		err := os.Mkdir(installLoc, 0755)
+		if err != nil {
+			log.Fatalln("error creating '/tmp/ygh' directory")
+		}
+	}
+
 	for _, pkg := range packageNames {
-		resp, err := http.Get("https://raw.githubusercontent.com/archlinux/aur/refs/heads/" + pkg + "/PKGBUILD")
+		url := fmt.Sprintf("https://raw.githubusercontent.com/archlinux/aur/refs/heads/%s/PKGBUILD", pkg)
+		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Println("error happened fetching PKGBUILD for " + pkg)
 		}
@@ -31,15 +45,58 @@ func main() {
 		}
 
 		var input string
-		fmt.Print("Do you want to read the PKGBUILD for " + pkg + "? [y/N]:")
+		fmt.Printf("Do you want to read the PKGBUILD for %s? [y/N]: ", pkg)
 		fmt.Scanln(&input)
 		switch input {
 		case "y", "Y":
-			fmt.Printf("%s \n", body)
+			pkgbuild := fmt.Sprintf("%s\n", body)
+			fmt.Printf("%s", pkgbuild)
 		case "N", "n", "":
-			continue
 		default:
-			fmt.Println("Option not recognized, defaulting to NO")
+			fmt.Println("Continue")
+		}
+
+		fmt.Println("Cloning the repo...")
+		if err := runCommand("git", "clone", "--branch", pkg, "--single-branch", "https://github.com/archlinux/aur.git", installLoc+"/"+pkg); err != nil {
+			fmt.Println("error happend cloning git repo")
+			fmt.Println(err)
+		}
+
+		fmt.Println("Building the package")
+		if err := runCommand("makepkg", "-D", installLoc+"/"+pkg); err != nil {
+			fmt.Println("error happend making the packages")
+			fmt.Println(err)
+		}
+
+		fmt.Println("Installing the package")
+		builtPkg := findPkgName(pkg, installLoc)
+		if err := runCommand("sudo", "pacman", "-U", installLoc+"/"+pkg+"/"+builtPkg); err != nil {
+			fmt.Println("error happend installing the packages")
+			fmt.Println(err)
 		}
 	}
+}
+
+func findPkgName(pkgName string, location string) string {
+	root := os.DirFS(location + "/" + pkgName)
+	pkgs, err := fs.Glob(root, "*.pkg.tar.zst")
+	if err != nil {
+		log.Fatalln("error happend locating built package", err)
+	}
+
+	for i, f := range pkgs {
+		if strings.Contains(f, "debug") {
+			pkgs = slices.Delete(pkgs, i, i)
+		}
+	}
+
+	return pkgs[0]
+}
+
+func runCommand(name string, args ...string) error {
+	command := exec.Command(name, args...)
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+	command.Stdin = os.Stdin
+	return command.Run()
 }
